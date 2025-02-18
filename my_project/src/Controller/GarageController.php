26421bb3a19d9a5861c635c7f79ec6f7cf84f689
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2025-02-18 15:06:21 +0100

    update

--- my_project/src/Controller/GarageController.php
+++ my_project/src/Controller/GarageController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AirExplorerController extends AbstractController
+final class GarageController extends AbstractController
 {
-    #[Route('/air/explorer', name: 'app_air_explorer')]
+    #[Route('/garage', name: 'app_garage')]
     public function index(): Response
     {
-        return $this->render('air_explorer/index.html.twig', [
-            'controller_name' => 'AirExplorerController',
+        return $this->render('garage/index.html.twig', [
+            'controller_name' => 'GarageController',
         ]);
     }
 }
