36f57b0141111113840118d2f7c320dd02ccd555
mgiovann <giovanni.mascaro@ynov.com>
2025-02-19 14:35:06 +0100

    login

--- my_project/src/Controller/HomeController.php
+++ my_project/src/Controller/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
-    }
+//     public function index(): Response
+//     {
+//         return $this->render('home/index.html.twig');
+//     }
+// }
+function index(EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response {
+    $user = new User();
+    $user->setEmail('gio@ma.fr')->setUsername('gio')->setPassword($hasher->hashPassword($user, 'gio'));
+    $em->persist($user);
+    $em->flush();
+    return $this->render('home/index.html.twig');
+}
 }
