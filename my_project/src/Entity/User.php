46e6c588d3c694fa9c6f7b80e72013f6ad43facb
mgiovann <giovanni.mascaro@ynov.com>
2025-02-20 15:04:50 +0100

    register

--- my_project/src/Entity/User.php
+++ my_project/src/Entity/User.php
@@ -34,9 +34,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $email = null;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -50,7 +47,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUsername(string $username): static
     {
         $this->username = $username;
-
         return $this;
     }
 
@@ -74,15 +70,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
-        if ($this->username === 'gio')
-        {
+        if ($this->username === 'gio') {
             $roles[] = 'ROLE_ADMIN';
         }
-        if ($this->isVerifi) {
-            $roles[] = 'ROLE_VERIFIED';
-        }
+
         return array_unique($roles);
-        
     }
 
     /**
@@ -91,7 +83,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
-
         return $this;
     }
 
@@ -106,7 +97,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
-
         return $this;
     }
 
@@ -117,17 +107,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
-
-        return $this;
     }
 }
