f1b9875adaf323c8847972de876657fe3ae35e07
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2025-02-20 16:25:29 +0100

    Update

--- my_project/src/Controller/CarController.php
+++ my_project/src/Controller/CarController.php
@@ -3,12 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-
 
 class CarController extends AbstractController
 {
@@ -27,25 +25,12 @@ class CarController extends AbstractController
         if ($id) {    
             if ($id == 1) {
                 $apiUrl = "http://127.0.0.1:5000/first30";
-
-                $response = $this->client->request('GET', $apiUrl);
-                $cars = $response->toArray();
-        
-                return $this->render('cars/cars.html.twig', [
-                    'cars' => $cars
-                ]);
             } else if ($id == 2) {
                 $apiUrl = "http://127.0.0.1:5000/cars/last33";
-
-                $response = $this->client->request('GET', $apiUrl);
-                $cars = $response->toArray();
-        
-                return $this->render('cars/cars.html.twig', [
-                    'cars' => $cars
-                ]);
             }
+        } else {
+            $apiUrl = "http://127.0.0.1:5000/cars/first30";
         }
-        $apiUrl = "http://127.0.0.1:5000/cars/first30";
 
         $response = $this->client->request('GET', $apiUrl);
         $cars = $response->toArray();
@@ -53,7 +38,23 @@ class CarController extends AbstractController
         return $this->render('cars/cars.html.twig', [
             'cars' => $cars
         ]);
+    }
 
+    #[Route('/cars/{id}', name: 'app_car', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        $apiUrl = "http://127.0.0.1:5000/cars/" . $id;
+
+        $response = $this->client->request('GET', $apiUrl);
         
+        if ($response->getStatusCode() !== 200) {
+            throw $this->createNotFoundException("Voiture non trouvée");
+        }
+
+        $car = $response->toArray();
+
+        return $this->render('cars/car_details.html.twig', [
+            'car' => $car
+        ]);
     }
 }
